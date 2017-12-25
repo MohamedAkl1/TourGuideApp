8a9665415cf01338ccc1c91f1cb1573f55c855a8
Mohamed Akl <mohamedakl96@gmail.com>
2017-12-26 01:39:32 +0200

    final project cleaning

--- app/src/main/java/com/example/akl/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/akl/tourguideapp/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle toggle;
     private NavigationView nvDrawer;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
